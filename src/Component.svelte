c6929e2886d82e07c148c9a92f99cd33e58e2c1a
DougA <douganderson444@gmail.com>
2020-10-23 15:55:42 -0400

    embed hypns bundle locally

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,7 +1,14 @@
 <script>
-  // Import browserify bundle (umd module)
-  import HyPNS from "../../HyPNS/browser/hypns-bundle.js" 
-
+  // Import rollup verison (es module)
+  // TODO: configure Rollup so that these work too
+  // import * as HyPNS from "HyPNS/dist/index.browser.js"; // Rollup UMD for browser; require is not defined
+  // import * as HyPNS from "HyPNS/browser/index.mjs"; // No native build (node-gyp-build)
+  // import * as HyPNS from "HyPNS"; // No native build (node-gyp-build)
+  
+  // Import browserify version (umd module)
+  import HyPNS from "../hypns-bundle.js" 
+  // import { hypns } from "../../HyPNS/browser/hypns-bundle" // WTF? 'hypns' is not exported by HyPNS
+  
   import { onMount, onDestroy } from "svelte";
 
   // passed down from Parent component
@@ -13,6 +20,7 @@
 
   onMount(() => {
     hypnsNode = new HyPNS(opts);
+    console.log('hypnsNode is: ', hypnsNode)
 
     // Close the HyPNS connection if the browser is closed
     window.addEventListener("unload", async (event) => {
